45ec13dfd54ac0892929449ff18f5bfb49c0a989
Brian Ginsburg <gins@brianginsburg.com>
2023-06-16 20:23:05 -0700

    Remove unneeded container

--- src/routes/avatars/+page.svelte
+++ src/routes/avatars/+page.svelte
@@ -3,7 +3,6 @@
 
   import { sessionStore } from '$src/stores'
   import { avatarsStore } from '$routes/avatars/stores'
-  import Container from '$routes/avatars/components/Container.svelte'
   import Summon from '$routes/avatars/components/Summon.svelte';
   import Transmogrify from '$routes/avatars/components/Transmogrify.svelte';
 
@@ -18,28 +17,26 @@
 <div class="mt-6 p-2 mb-14">
   <h1 class="text-heading-lg mb-4">ODD Avatar Generator</h1>
   {#if $sessionStore.session}
-  <div class="flex justify-content-start mb-8">
-    <div class="tabs border-2 overflow-hidden border-base-content">
+    <div class="flex justify-content-start mb-8">
+      <div class="tabs border-2 overflow-hidden border-base-content">
         {#each AREAS as area}
           <button
             on:click={() => handleChangeTab(area)}
             class="tab font-sans uppercase text-heading-sm ease-in {$avatarsStore.selectedArea ===
             area
-            ? 'tab-active bg-base-content text-base-100'
+              ? 'tab-active bg-base-content text-base-100'
               : 'bg-base-100 text-base-content'}"
           >
             {area}
           </button>
         {/each}
+      </div>
     </div>
-  </div>
 
-    <Container>
-      {#if $avatarsStore.selectedArea === 'Transmogrify'}
-        <Transmogrify />
-      {:else if $avatarsStore.selectedArea === 'Summon'}
-        <Summon />
-      {/if}
-    </Container>
+    {#if $avatarsStore.selectedArea === 'Transmogrify'}
+      <Transmogrify />
+    {:else if $avatarsStore.selectedArea === 'Summon'}
+      <Summon />
+    {/if}
   {/if}
 </div>
--- src/routes/avatars/components/Container.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-</script>
-
-<label
-  class="block w-full min-h-[calc(100vh-190px)] transition ease-in"
->
-  <slot />
-</label>
