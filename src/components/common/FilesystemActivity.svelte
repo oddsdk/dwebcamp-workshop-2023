12c950415b9f8064a37d14b454ed49df9297c985
Brian Ginsburg <gins@brianginsburg.com>
2023-06-20 14:05:21 -0700

    Center file system activity messages

--- src/components/common/FilesystemActivity.svelte
+++ src/components/common/FilesystemActivity.svelte
@@ -3,12 +3,12 @@
 </script>
 
 <div class="flex min-h-[80vh] min-w-full justify-center items-center">
-  <div class="min-w-[50vw] flex flex-row items-center">
-      <span
-        class="rounded-full border-4 border-t-4 border-t-base-content border-base-100 w-8 h-8 inline-block animate-spin mr-4"
-      ></span>
-      <h3 class="text-heading-lg">
-        {activity} file system...
-      </h3>
+  <div class="min-w-[50vw] flex flex-row justify-center items-center">
+    <span
+      class="rounded-full border-4 border-t-4 border-t-base-content border-base-100 w-8 h-8 inline-block animate-spin mr-4"
+    />
+    <h3 class="text-heading-lg">
+      {activity} file system...
+    </h3>
   </div>
 </div>
